d2c0fc84e8797f38a63a92651632e4b652043054
sam-russett <sam@russetts.com>
2023-05-05 19:45:21 -0500

    initial final project commit

---
+++ backend/data/products.js
@@ -0,0 +1,62 @@
+
+const products = [
+
+    {
+        name: "Amazon Fire TV 50 inch",
+        imageUrl: "https://m.media-amazon.com/images/I/61vM5Z-bBFL._AC_SX679_.jpg",
+        description: "Amazon Fire TV 50\" Omni Series 4K UHD smart TV, hands-free with Alexa",
+        price: 309.99,
+        countInStock: 50
+    },
+    {
+        name: "Amazon Fire TV 43 inch",
+        imageUrl: "https://m.media-amazon.com/images/I/716b+5ZKQpL._AC_SY300_SX300_.jpg",
+        description: "Amazon Fire TV 43\" 4-Series 4K UHD smart TV",
+        price: 249.99,
+        countInStock: 50
+    },
+    {
+        name: "Insignia TV",
+        imageUrl: "https://m.media-amazon.com/images/I/815E5aShTqL._AC_SX466_.jpg",
+        description: "INSIGNIA 32-inch Class F20 Series Smart HD 720p Fire TV (NS-32F201NA23, 2022 Model)",
+        price: 109.99,
+        countInStock: 50
+    },
+    {
+        name: "Samsung Galaxy Watch",
+        imageUrl: "https://m.media-amazon.com/images/I/51v3H5bDqPL.__AC_SY300_SX300_QL70_FMwebp_.jpg",
+        description: "SAMSUNG Galaxy Watch 5 40mm LTE Smartwatch w/ Body, Health, Fitness and Sleep Tracker, Improved Battery, Sapphire Crystal Glass, Enhanced GPS Tracking, US Version, Pink Gold Bezel w/ Pink Band",
+        price: 329.99,
+        countInStock: 50
+    },
+    {
+        name: "Samsung Galaxy Watch Pro",
+        imageUrl: "https://m.media-amazon.com/images/I/61Sl+xoVHoL._AC_SX466_.jpg",
+        description: "SAMSUNG Galaxy Watch Pro 5 45mm LTE Smartwatch w/ Body, Health, Fitness and Sleep Tracker, Improved Battery, Sapphire Crystal Glass, GPS Route Tracking, Titanium Frame, US Version, Black",
+        price: 449.00,
+        countInStock: 50
+    },
+    {
+        name: "Amazon Fire Tablet",
+        imageUrl: "https://m.media-amazon.com/images/I/61fgc1OW1ZL._AC_SX679_.jpg",
+        description: "Amazon Fire HD 8 tablet, 8” HD Display, 32 GB, updated processor, great for movies and books, latest model (2022 release), Black.",
+        price: 59.99,
+        countInStock: 50
+    },
+    {
+        name: "Lenovo Laptop",
+        imageUrl: "https://m.media-amazon.com/images/I/61O8GIu6obL._AC_SX466_.jpg",
+        description: "Lenovo 15.6\" IdeaPad 1 Laptop, AMD Dual-core Processor, 15.6\" HD Anti-Glare Display, Wi-Fi 6 and Bluetooth 5.0, HDMI, Windows 11 Home in S Mode(20GB RAM | 1TB SSD)",
+        price: 399.00,
+        countInStock: 50
+    },
+    {
+        name: "HP Notebook",
+        imageUrl: "https://m.media-amazon.com/images/I/71NimEyVtXL._AC_SX466_.jpg",
+        description: "HP 15 Notebook, 15.6\" HD Screen, Intel Core i3-1115G4, 12GB DDR4 RAM, 256GB PCIe SSD, Webcam, HDMI, SD Card Reader, Wi-Fi, Windows 11 Home, Natural Silver",
+        price: 399.00,
+        countInStock: 50
+    }
+];
+
+module.exports = products;
